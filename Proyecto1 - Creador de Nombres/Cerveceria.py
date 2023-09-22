811df540bb1974d226d06125e73f8c86b4eb2789
Julian Zanetti <julianzanetti99@gmail.com>
2023-09-21 21:18:00 -0300

    Primer commit

---
+++ Proyecto1 - Creador de Nombres/Cerveceria.py
@@ -0,0 +1,4 @@
+# Brindar un nombre de cerveza realizando preguntas random al usuario
+nombre1 = input("Que ciudad te gustaria visitar? ")
+nombre2 = input("Cual es tu color favorito? ")
+print(f"El nombre de tu cerveza es {nombre1} {nombre2}")
