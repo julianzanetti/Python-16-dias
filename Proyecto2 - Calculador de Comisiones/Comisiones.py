fb1655bf924274652e241813fdc191ce16a656da
Julián <134458575+julianzanetti@users.noreply.github.com>
2024-04-09 19:40:28 -0300

    Update Comisiones.py

--- Proyecto2 - Calculador de Comisiones/Comisiones.py
+++ Proyecto2 - Calculador de Comisiones/Comisiones.py
@@ -1,4 +1,4 @@
 nombre = input("Ingrese su nombre: ")
-ventas = float(input("Ingrese las ventas del mes: "))
+ventas = float(input("Ingrese las ventas totales del mes: "))
 calculo = ventas * 0.13
 print(f"Hola {nombre}, tus comisiones en este mes son de {round(calculo, 1)}")
