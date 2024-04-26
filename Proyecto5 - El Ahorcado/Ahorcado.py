adaa7796774f2bdda19cc3d938dced068c790020
Julián <134458575+julianzanetti@users.noreply.github.com>
2024-04-25 23:04:12 -0300

    Update Ahorcado.py

--- Proyecto5 - El Ahorcado/Ahorcado.py
+++ Proyecto5 - El Ahorcado/Ahorcado.py
@@ -9,7 +9,7 @@ def validar_letra(letra):
     if letra.isalpha() and len(letra) == 1:
         return letra
     elif letra.isdigit():
-        print(f"ERROR! No esta permitido numeros.")
+        print(f"ERROR! No esta permitido ingresar numeros")
     else:
         print(f"ERROR! Ingresaste una cadena de caracteres")
 
@@ -19,12 +19,19 @@ def ingreso_letra():
         if validar_letra(letra):
             return letra
 
-def chequear_letra(letra_mostrar):
-    for indice in range(1, len(palabras)):
-        if palabras[indice] == letra_mostrar:
-            return True
-        else:
-            return False
+def chequear_letra(letra):
+    if letra in palabras:
+        print("")
+        print("-"*50)
+        print(f"Muy bien! Ingresaste una letra correcta.")
+        letras_correctas.append(letra)
+        return True
+    else:
+        print("")
+        print("-"*50)
+        print(f"Ups! Ingresaste una letra incorrecta")
+        letras_incorrectas.append(letra)
+        return False
 
 def validacion():
     palabra_incompleta = palabras[0]
@@ -38,28 +45,25 @@ def validacion():
 
 print(f"¡Bienvenido al ahorcado!".center(50, "-"))
 print(f"Tienes {vidas} vidas para intentar adivinar la palabra oculta")
-while vidas > 0:
-    print(f"La palabra oculta es: {validacion()}")
+while vidas != 0:
+    print(f"La palabra oculta es {validacion()}")
     ingreso = ingreso_letra()
     chequeo = chequear_letra(ingreso)
     if chequeo == True:
-        print("\n-----------------------------------------------------------------------")
-        print(f"Muy bien! Ingresaste una letra correcta.")
-        letras_correctas.append(ingreso)
+        print(f"Te quedan {vidas} vidas")
+        print("-" * 50)
+        print("")
     else:
-        print("\n-----------------------------------------------------------------------")
-        print(f"Ups! Ingresaste una letra incorrecta")
-        letras_incorrectas.append(ingreso)
         vidas -= 1
+        print(f"Te quedan {vidas} vidas")
+        print("-" * 50)
+        print("")
     if validacion() == palabras:
         print("")
         print(f"¡GANASTE!".center(50,"-"))
-        print(f"La palabra completa es: {palabras}")
+        print(f"Adivinaste la palabra oculta que era {palabras}")
         break
-    else:
-        print(f"Te quedan {vidas} vidas restantes.")
-        print(f"Hasta ahora ingresaste estas letras incorrectamente: {letras_incorrectas}")
-        print("-----------------------------------------------------------------------\n")
+
 else:
-    print(f"PERDISTE".center(50, "-"))
-    print(f"La palabra completa era: {palabras}")
+    print(f"GAME OVER!")
+    print(f"La palabra oculta era {palabras}")
