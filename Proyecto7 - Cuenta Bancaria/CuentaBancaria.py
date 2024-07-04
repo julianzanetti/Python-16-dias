76acb9b3781124beb705818983cf17c2378ca821
Julián <134458575+julianzanetti@users.noreply.github.com>
2024-07-04 20:53:11 -0300

    Proyecto del dia 7

--- Proyecto7 - Cuenta Bancaria/CuentaBancaria.py
+++ Proyecto7 - Cuenta Bancaria/CuentaBancaria.py
@@ -1,74 +1,105 @@
-class Persona():
+import random
+import os
+
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+class Persona:
     def __init__(self, nombre, apellido):
-        self.nombre = nombre
-        self.apellido = apellido
+        self.nom = nombre
+        self.ape = apellido
+
 
 class Cliente(Persona):
-    def __init__(self, nombre, apellido, nro_cuenta, balance = 0):
+    def __init__(self, nombre, apellido, nro_cta, balance=0):
         super().__init__(nombre, apellido)
-        self.nro_cuenta = nro_cuenta
+        self.nroCta = nro_cta
         self.balance = balance
 
     def __str__(self):
-        return f"Cliente: {self.nombre} {self.apellido}\nBalance de cuenta {self.nro_cuenta}: {self.balance}"
+        return f"""        Nombre: {self.nom}
+        Apellido: {self.ape}
+        Nro. CtaCte: {self.nroCta}
+        Balance de la Cta: {self.balance}"""
 
     def depositar(self, deposito):
-        self.balance += deposito
-        print(f"¡Ingreso exitoso!")
+        if deposito > 0:
+            self.balance += deposito
+            limpiar_pantalla()
+        else:
+            limpiar_pantalla()
+            print("ERROR! No puedes depositar una cantidad negativa o cero.")
 
     def retirar(self, retiro):
-        if self.balance == 0:
-            print(f"Tienes que depositar primero.")
-        elif self.balance >= retiro:
-            self.balance -= retiro
-            print(f"¡Retiro exitoso!")
+        if retiro > 0:
+            if self.balance < retiro:
+                limpiar_pantalla()
+                print(f"ERROR! No puedes retirar más de lo que posees!")
+            else:
+                self.balance -= retiro
+                limpiar_pantalla()
         else:
-            print(f"Fondos Insuficiente")
+            limpiar_pantalla()
+            print("ERROR! No puedes retirar una cantidad negativa o cero.")
+
+
+def bienvenida():
+    print("-"*50)
+    print("Bienvenido al Banco PYTHON".center(50))
+    print("-"*50)
 
 
 def crear_cliente():
     nombre = input("Ingrese su nombre: ")
     apellido = input("Ingrese su apellido: ")
-    nro_cuenta = int(input("Ingrese su numero de cuenta: "))
+    numeros_aleatorios = [str(random.randint(0, 9)) for _ in range(8)]
+    nro_cuenta = int(''.join(numeros_aleatorios))
     cliente = Cliente(nombre, apellido, nro_cuenta)
     return cliente
 
 
-def menu():
-    print(f"Cuenta Bancaria".center(50, "-"))
-    print("1- Depositar")
-    print("2- Retirar")
-    print("3- Salir")
+def menu(cliente):
     while True:
+        print("-" * 50)
+        print(cliente)
+        print("-" * 50)
+        print("1- Depositar")
+        print("2- Retirar")
+        print("3- Salir")
         try:
-            opcion = int(input("Ingrese una opcion: "))
-            if opcion > 3:
-                print(f"ERROR! Ingresaste un numero mayor a 3.")
-            elif opcion < 1:
-                print(f"ERROR! Ingresaste un numero menor a 1.")
+            opcion = int(input("Elija una operación: "))
+            if opcion not in [1, 2, 3]:
+                limpiar_pantalla()
+                print("ERROR! Ingresaste una opción no válida.")
             else:
                 return opcion
-        except ValueError as e:
-            print(e)
-            print(f"ERROR! No esta permitido caracteres")
+        except ValueError:
+            limpiar_pantalla()
+            print("ERROR! No está permitido ingresar caracteres no numéricos.")
 
 
-opcion = 0
+bienvenida()
 cliente = crear_cliente()
+limpiar_pantalla()
+opcion = 0
 while opcion != 3:
-    print("*" * 50)
-    print(cliente)
-    print("*" * 50)
-    print("")
-    opcion = menu()
+    opcion = menu(cliente)
     if opcion == 1:
-        monto_dep = int(input("Monto a depositar: "))
-        cliente.depositar(monto_dep)
+        try:
+            monto_deposito = int(input("Ingrese la cantidad a depositar: "))
+            cliente.depositar(monto_deposito)
+        except ValueError:
+            limpiar_pantalla()
+            print("ERROR! Debes ingresar un número.")
     elif opcion == 2:
-        monto_ret = int(input("Monto a retirar: "))
-        cliente.retirar(monto_ret)
+        try:
+            monto_retiro = int(input("Ingrese la cantidad a retirar: "))
+            cliente.retirar(monto_retiro)
+        except ValueError:
+            limpiar_pantalla()
+            print("ERROR! Debes ingresar un número.")
 else:
-    print("*" * 50)
-    print(cliente)
-    print("*" * 50)
-    print(f"Gracias por operar en nuestro banco")
+    limpiar_pantalla()
+    print("Gracias por utilizar el Banco PYTHON!")
