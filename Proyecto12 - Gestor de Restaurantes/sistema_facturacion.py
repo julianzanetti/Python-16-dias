91241cef5f66f35d9805b7cd9d3e8f4a003637e2
Julián <134458575+julianzanetti@users.noreply.github.com>
2024-08-23 18:32:15 -0300

    Se agrega los chekbuttons

--- Proyecto12 - Gestor de Restaurantes/sistema_facturacion.py
+++ Proyecto12 - Gestor de Restaurantes/sistema_facturacion.py
@@ -59,5 +59,41 @@ panel_recibo.pack()
 panel_botones = Frame(panel_derecho, bd=1, relief=FLAT, bg='burlywood')
 panel_botones.pack()
 
+# Lista de Productos
+lista_comidas = ['pollo', 'cordero', 'salmon', 'merluza', 'hamburguesa', 'pizza1', 'pizza2', 'pizza3']
+lista_bebidas = ['agua', 'soda', 'cola', 'jugo', 'cerveza1', 'cerveza2', 'vino1', 'vino2']
+lista_postres = ['helado', 'flan', 'tiramisu', 'brownie', 'lemon pie', 'mousse', 'budin', 'Torta']
+
+# Generar el Item de comidas
+variables_comida = []
+contador = 0
+for comida in lista_comidas:
+    variables_comida.append('')
+    variables_comida[contador] = IntVar()
+    comida = Checkbutton(panel_comidas, text=comida.title(), font=('Dosis', 19, 'bold'), onvalue=1, offvalue=0, variable=variables_comida[contador])
+    comida.grid(row=contador, column=0, sticky=W)
+    contador +=1
+
+# Generar el Item de bebidas
+variables_bebidas = []
+contador = 0
+for bebida in lista_bebidas:
+    variables_bebidas.append('')
+    variables_bebidas[contador] = IntVar()
+    bebida = Checkbutton(panel_bebidas, text=bebida.title(), font=('Dosis', 19, 'bold'), onvalue=1, offvalue=0, variable=variables_bebidas[contador])
+    bebida.grid(row=contador, column=0, sticky=W)
+    contador +=1
+
+# Generar el Item de postres
+variables_postres = []
+contador = 0
+for postre in lista_postres:
+    variables_postres.append('')
+    variables_postres[contador] = IntVar()
+    postre = Checkbutton(panel_postres, text=postre.title(), font=('Dosis', 19, 'bold'), onvalue=1, offvalue=0, variable=variables_postres[contador])
+    postre.grid(row=contador, column=0, sticky=W)
+    contador +=1
+
+
 # Evitar que la pantalla se cierre
 aplicacion.mainloop()
