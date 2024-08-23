da4833e3586f4346232ac19c2a21836cee83caa5
Julián <134458575+julianzanetti@users.noreply.github.com>
2024-08-23 17:52:36 -0300

    Se crea la ventana

--- Proyecto12 - Gestor de Restaurantes/sistema_facturacion.py
+++ Proyecto12 - Gestor de Restaurantes/sistema_facturacion.py
@@ -1,1 +1,19 @@
+from tkinter import *
 
+# Iniciar tkinter
+aplicaion = Tk()
+
+# Tamaño de la ventana
+aplicaion.geometry('1020x630+0+0')
+
+# Evitar maximizar
+aplicaion.resizable(0,0)
+
+# Titulo de la app
+aplicaion.title("Sistema de Facturacion")
+
+# Color del fondo
+aplicaion.config(bg="burlywood")
+
+# Evitar que la pantalla se cierre
+aplicaion.mainloop()
