88620cc2b67fb8b1b3021e1050e414e6f4b57319
Julián <134458575+julianzanetti@users.noreply.github.com>
2025-05-19 22:33:17 -0300

    Update views.py

--- Proyecto16 - Aplicacion Web de Tares Pendientes/base/views.py
+++ Proyecto16 - Aplicacion Web de Tares Pendientes/base/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from base.models import Tarea
-
 
 class ListaPendientes(ListView):
     model = Tarea
     context_object_name = 'tareas'
+
+class DetalleTarea(DetailView):
+    model = Tarea
+    context_object_name = 'tarea.detalle'
