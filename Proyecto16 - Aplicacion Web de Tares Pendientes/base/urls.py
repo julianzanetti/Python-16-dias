ed180bf9aa3acef0fb807696837e866d4ac02da9
Julián <134458575+julianzanetti@users.noreply.github.com>
2025-05-23 22:14:12 -0300

    Add files via upload

--- Proyecto16 - Aplicacion Web de Tares Pendientes/base/urls.py
+++ Proyecto16 - Aplicacion Web de Tares Pendientes/base/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from . import views
+from .views import ListaPendientes, DetalleTarea, CrearTarea, UpdateTarea, DeleteTarea, Logueo, PaginaRegistro
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", ListaPendientes.as_view(), name="pendientes"),
+    path("tarea/<int:pk>", DetalleTarea.as_view(), name="detalle-tarea"),
+    path("crear-tarea/", CrearTarea.as_view(), name="crear-tarea"),
+    path("editar-tarea/<int:pk>", UpdateTarea.as_view(), name="editar-tarea"),
+    path("eliminar-tarea/<int:pk>", DeleteTarea.as_view(), name="eliminar-tarea"),
+    path("login/", Logueo.as_view(), name="login"),
+    path("registro/", PaginaRegistro.as_view(), name="registro"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
 ]
